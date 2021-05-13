08d5128047fae3006566b68a457be482ee72d7a9
PanchoPC2 <58098908+Panchober27@users.noreply.github.com>
2021-05-12 23:07:12 -0400

    Subida a Github

---
+++ app/src/main/java/com/example/trabajo2_appmoviles_francisco_berwart/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.trabajo2_appmoviles_francisco_berwart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
